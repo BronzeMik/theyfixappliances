204b9468dd974d32e1daaa7a2860c37266d2212f
Mikaila Brown <mikaila.brown2022@gmail.com>
2024-07-29 10:10:04 -0400

    Update logo size

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -4,8 +4,8 @@ import webpimg from '../assets/tarc-logo.webp'
 export default function Logo() {
     return(
 
-        <div className='max-w-[6%]'>
-            <picture className='md:max-w-[40%]'>
+        <div className='max-w-[20%] md:max-w-[6%]'>
+            <picture className=' md:max-w-[40%]'>
                     <source srcSet={webpimg} type="image/webp" />
                     <img src={pngimg} alt="Description of the image" title='The Appliance Repair Centers'/>
             </picture>
